00b542569a888a0fe4fa19f7a5b576ae889a8322
Daniel <danielsoh123@gmail.com>
2021-11-23 21:27:50 +0800

    update server

--- server.js
+++ server.js
@@ -14,7 +14,10 @@ app.use(express.urlencoded({ extended: true }));
 
 // enabling cors for stripe api for payments
 app.use(function (req, res, next) {
-	res.header("Access-Control-Allow-Origin", "http://localhost:3000");
+	res.header(
+		"Access-Control-Allow-Origin",
+		"https://crwnclothing-dandemo.herokuapp.com/"
+	);
 	res.header(
 		"Access-Control-Allow-Headers",
 		"Origin, X-Requested-With, Content-Type, Accept"
